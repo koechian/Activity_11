31a19df93c0172567e3cf2ac5f5247480662cdf9
Ian Koech <ianovic8@live.com>
2021-11-22 23:02:11 +0300

    new error 404 page

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -1,84 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<title>404 Page Not Found</title>
-
-	<style>
-		div.logo {
-			height: 200px;
-			width: 155px;
-			display: inline-block;
-			opacity: 0.08;
-			position: absolute;
-			top: 2rem;
-			left: 50%;
-			margin-left: -73px;
-		}
-		body {
-			height: 100%;
-			background: #fafafa;
-			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-			color: #777;
-			font-weight: 300;
-		}
-		h1 {
-			font-weight: lighter;
-			letter-spacing: 0.8;
-			font-size: 3rem;
-			margin-top: 0;
-			margin-bottom: 0;
-			color: #222;
-		}
-		.wrap {
-			max-width: 1024px;
-			margin: 5rem auto;
-			padding: 2rem;
-			background: #fff;
-			text-align: center;
-			border: 1px solid #efefef;
-			border-radius: 0.5rem;
-			position: relative;
-		}
-		pre {
-			white-space: normal;
-			margin-top: 1.5rem;
-		}
-		code {
-			background: #fafafa;
-			border: 1px solid #efefef;
-			padding: 0.5rem 1rem;
-			border-radius: 5px;
-			display: block;
-		}
-		p {
-			margin-top: 1.5rem;
-		}
-		.footer {
-			margin-top: 2rem;
-			border-top: 1px solid #efefef;
-			padding: 1em 2em 0 2em;
-			font-size: 85%;
-			color: #999;
-		}
-		a:active,
-		a:link,
-		a:visited {
-			color: #dd4814;
-		}
-	</style>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Error 404 | GUSHI</title>
 </head>
-<body>
-	<div class="wrap">
-		<h1>404 - File Not Found</h1>
 
-		<p>
-			<?php if (! empty($message) && $message !== '(null)') : ?>
-				<?= nl2br(esc($message)) ?>
-			<?php else : ?>
-				Sorry! Cannot seem to find the page you were looking for.
-			<?php endif ?>
-		</p>
-	</div>
+<body>
+  <div class="side-img">
+    <img src="/Assets/error.jpg" alt="Damn we really are having a trash day..." />
+  </div>
+  <div class="wrapper">
+    <p id="error_text">ERROR 404</p>
+    <p>
+      Sorry,our bad. We may or may not <br />behaving that page at the moment.
+    </p>
+    <div class="home">
+      <span>Country Road...</span>
+      <button>
+        <a href="<?= site_url('Pages') ?>">Take me home...</a>
+      </button>
+    </div>
+  </div>
+
+  <style>
+    * {
+      margin: 0;
+      box-sizing: border-box;
+      padding: 0;
+      font-family: "Hack";
+    }
+
+    .side-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+
+    img {
+      height: 100%;
+      width: 731px;
+    }
+
+    .wrapper {
+      position: absolute;
+      width: 1027px;
+      height: 100%;
+      top: 0;
+      right: 0;
+    }
+
+    #error_text {
+      font-size: 50px;
+      margin-top: 200px;
+      margin-left: 100px;
+    }
+
+    p {
+      font-size: 25px;
+      margin-top: 200px;
+    }
+
+    .home {
+      margin-top: 200px;
+      font-size: 25px;
+    }
+
+    .home button {
+      background: #404040;
+      border: none;
+      border-radius: 10px;
+      height: 50px;
+      width: 200px;
+      transition: ease 0.4s;
+    }
+
+    .home button a {
+      text-decoration: none;
+      color: #d6d6d6;
+      font-size: 20px;
+      transition: ease 0.2s;
+    }
+
+    .home button:hover a,
+    .home button:hover {
+      background: #c9c9c9;
+      color: #181818;
+    }
+  </style>
 </body>
+
 </html>
